ae166fcd2c6cf20174a3e706772ca1c23469c3c1
Aellas <zacpi@pm.me>
2025-08-08 17:48:51 +0100

    Update bolt.nix

--- modules/programs/bolt.nix
+++ modules/programs/bolt.nix
@@ -1,18 +1,18 @@
-  { pkgs, ... }:
+{ pkgs, ... }:
 {
-    programs.bolt-launcher = {
-        enable = true;
-    };
-  
-  nixpkgs.overlays = [
-  (self: super: {
-    bolt-launcher = super.bolt-launcher.override {
-      enableRS3 = true;
-      };
-    })
-  ];
+  #nixpkgs.overlays = [
+   # (self: super: {
+    #  bolt-launcher = super.bolt-launcher.override {
+    #    enableRS3 = true;
+    #  };
+    # })
+  #];
 
   nixpkgs.config.permittedInsecurePackages = [
-   "openssl-1.1.1w"
+    "openssl-1.1.1w"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    bolt-launcher
   ];
 }
