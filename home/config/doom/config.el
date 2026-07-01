3395b93429db0cd952cc4aa2c572cec4ec527ded
array <array@thinkfor.home>
2026-07-01 19:17:01 +0100

    updated

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -1,6 +1,7 @@
-;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;;; UI & Fonts
+
 (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 25)
       doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font")
       doom-theme 'modern-catppuccin-latte
@@ -13,68 +14,74 @@
 (add-hook 'tty-setup-hook #'xterm-mouse-mode)
 
 ;;; Dashboard
+
 (use-package! dashboard
+  :init
+  (dashboard-setup-startup-hook)
   :config
-  (setq 
-   dashboard-navigation-cycle t
-   dashboard-week-agenda t
-   dashboard-agenda-prefix-format " %-4s - %t"
-   dashboard-agenda-format-item-string "%s"
-   dashboard-projects-backend 'projectile
-   dashboard-startup-banner "~/.config/doom/ascii.txt"
-   dashboard-center-content t
-   dashboard-items '((recents   . 7)
-                     (projects  . 5)
-                     (bookmarks . 3)
-                     (agenda    . 5))
-   dashboard-set-heading-icons t
-   dashboard-set-file-icons t
-   dashboard-icon-type 'nerd-icons
-   dashboard-startupify-list '(dashboard-insert-banner
-                               dashboard-insert-navigator
-                               dashboard-insert-newline
-                               dashboard-insert-items
-                               dashboard-insert-newline))
+  (setq dashboard-startup-banner "~/.config/doom/ascii.txt"
+        dashboard-center-content t
+        dashboard-items '((recents . 7)
+                          (projects . 5)
+                          (bookmarks . 3)
+                          (agenda . 5))
+        dashboard-navigation-cycle t
+        dashboard-set-heading-icons t
+        dashboard-set-file-icons t
+        dashboard-icon-type 'nerd-icons))
 ;;; Dired & Treemacs
-  (setq dired-listing-switches "-alh --group-directories-first"
-        treemacs-show-hidden-files t)
+
+(setq dired-listing-switches "-alh --group-directories-first"
+      treemacs-show-hidden-files t)
 
 ;;; Org
-  (setq org-directory "~/org/")
 
-  (after! org
-    (setq org-hide-emphasis-markers t
-          org-pretty-entities t
-          org-adapt-indentation t
-          org-src-fontify-natively t
-          org-src-tab-acts-natively t
-          org-edit-src-content-indentation 0
-          org-agenda-prefix-format '((agenda . "  %i %-12:c%?-12t% s")
-                                     (todo   . "  %i %-12:c")
-                                     (tags   . "  %i %-12:c")
-                                     (search . "  %i %-12:c"))
-          org-agenda-files '("~/org/")))
+(setq org-directory "~/org/")
 
-  ;; Olivetti
-  (use-package! olivetti
-    :hook (org-mode . olivetti-mode)
-    :config
-    (setq olivetti-body-width 100))
+(after! org
+  (setq org-hide-emphasis-markers t
+        org-pretty-entities t
+        org-adapt-indentation t
+        org-src-fontify-natively t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 0
+        org-agenda-prefix-format
+        '((agenda . "  %i %-12:c%?-12t% s")
+          (todo   . "  %i %-12:c")
+          (tags   . "  %i %-12:c")
+          (search . "  %i %-12:c"))
+        org-agenda-files '("~/org/")))
 
-  (use-package! org-modern
-    :hook (org-mode . org-modern-mode)
-    :config
-    (setq org-modern-star 'replace
-          org-modern-hide-stars t
-          org-modern-table t
-          org-modern-block-fringe t))
+;;; Olivetti
+
+(use-package! olivetti
+  :hook (org-mode . olivetti-mode)
+  :config
+  (setq olivetti-body-width 100))
+
+;;; Org Modern
+
+(use-package! org-modern
+  :hook (org-mode . org-modern-mode)
+  :config
+  (setq org-modern-star 'replace
+        org-modern-hide-stars t
+        org-modern-table t
+        org-modern-block-fringe t))
 
 ;;; Tree-sitter
-  (setq treesit-language-source-alist
-        '((toml "https://github.com/tree-sitter/tree-sitter-toml")))
 
-  (after! flycheck (setq flycheck-idle-change-delay 0.1))
-  (after! lsp-mode
-    (setq lsp-idle-delay 0.1)
-    (setq lsp-completion-enable-additional-text-edit t)
-    (setq lsp-modeline-code-actions-enable t)))
+(setq treesit-language-source-alist
+      '((toml "https://github.com/tree-sitter/tree-sitter-toml")))
+
+;;; Flycheck
+
+(after! flycheck
+  (setq flycheck-idle-change-delay 0.1))
+
+;;; LSP
+
+(after! lsp-mode
+  (setq lsp-idle-delay 0.1
+        lsp-completion-enable-additional-text-edit t
+        lsp-modeline-code-actions-enable t))
