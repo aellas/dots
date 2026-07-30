bb1ad89871a1ff6d9350276b81a6efc7a952b976
lxde <zacpi@pm.me>
2026-07-30 11:28:54 +0100

    Update modules/git.nix

--- modules/git.nix
+++ modules/git.nix
@@ -1,12 +1,12 @@
 {
-  config,
-  lib,
-  pkgs,
   ...
 }:
-
 {
   programs.git = {
     enable = true;
+    settings = {
+      user.name = "lxde";
+      user.email = "zacpi@pm.me";
+    };
   };
 }
