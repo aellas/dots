71f6dae1be34823cfbf3118081cda87ebde8e12e
lxde <zacpi@pm.me>
2026-08-03 19:06:11 +0100

    adding gtk

---
+++ modules/gtk.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+
+{
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "WhiteSur-Light";
+      package = pkgs.whitesur-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "Paprius";
+      package = pkgs.whitesur-icon-theme;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 0;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 0;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3";
+    style.name = "adwaita";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "default";
+      gtk-theme = "WhiteSur-Light";
+      icon-theme = "Paprius";
+    };
+  };
+
+  home.packages = with pkgs; [
+    whitesur-gtk-theme
+    gnome-themes-extra
+    papirus-icon-theme
+  ];
+}
