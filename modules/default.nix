d72c6d21984a6eb70770b5818dfad68d079de814
lxde <zacpi@pm.me>
2026-08-04 15:21:24 +0100

    updated

--- modules/default.nix
+++ modules/default.nix
@@ -4,14 +4,14 @@
 
 {
   imports = [
-    ./fonts.nix
-    ./git.nix
-    ./zsh.nix
     ./emacs.nix
-    ./gtk.nix
 
     ./browsers/floorp.nix
     ./browsers/helium.nix
     ./terminals/wezterm.nix
+    ./shells/zsh.nix
+    ./system/fonts.nix
+    ./system/gtk.nix
+    ./system/git.nix
   ];
 }
