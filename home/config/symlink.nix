e69810b687442a91b0aabc8c56df4a155bb4ff9f
lxde <zacpi@pm.me>
2026-08-04 20:46:17 +0100

    updated

---
+++ home/config/symlink.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/nux/home/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    doom = "doom";
+    fastfetch = "fastfetch";
+    wezterm = "wezterm";
+    bspwm = "bspwm";
+    polybar = "polybar";
+    sxhkd = "sxhkd";
+    rofi = "rofi";
+    dunst = "dunst";
+    picom = "picom";
+  };
+in
+
+{
+  xdg.configFile = builtins.mapAttrs (name: subpath: {
+    source = create_symlink "${dotfiles}/${subpath}";
+    recursive = true;
+  }) configs;
+
+}
